9c2c453a8014b981cd5d4586773325e807cdcb50
jobjab <jab.for.j@gmail.com>
2022-01-19 01:08:15 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const Firebase = require('./firebase/config.js')
+const firebase = new Firebase();
+const cors = require('cors')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+app.get('/AllUser', async function(req, res, next) {
+  try{
+    const alluser = await firebase.getAlluser();
+    res.status(200).json(alluser);  
+  }
+  catch(e){
+    res.status(200).json(e);  
+  }
+});
+app.post('/token', async function(req, res, next) {
+  try{
+    const uid = req.body.uid;
+    const token = await firebase.generateToken(uid);
+    res.status(200).json(token);  
+  }
+  catch(e){
+    res.status(200).json(e);  
+  }
+});
+
+app.listen(9000, () => {
+  console.log('Start server at port 9000.')
+})
