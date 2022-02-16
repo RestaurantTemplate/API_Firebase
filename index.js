ab828f998517131f325656a16fbe5893a604108e
jobjab <jab.for.j@gmail.com>
2022-02-17 02:50:07 +0700

    Commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World 111')
 })
 app.post('/AddUser', async function(req, res, next) {
   try{
